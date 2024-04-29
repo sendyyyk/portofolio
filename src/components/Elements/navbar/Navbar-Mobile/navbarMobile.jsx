94a45d535f1c577c2a554b9fdcf46527e3093e4d
sendyyyk <sendy.2k18@gmail.com>
2024-04-29 18:37:46 +0700

    hahaha

--- src/components/Elements/navbar/Navbar-Mobile/navbarMobile.jsx
+++ src/components/Elements/navbar/Navbar-Mobile/navbarMobile.jsx
@@ -40,7 +40,7 @@ const NavbarMobile = () => {
 
     menuMbEx = menuMb
     return (
-        <div ref={navMb} id="navbar-mobile" className={`hidden absolute left-0 right-0 top-0 bottom-0 w-full bg-red-400`}>
+        <div ref={navMb} id="navbar-mobile" className={`hidden absolute left-0 right-0 top-0 bottom-0 w-full`}>
             <div ref={menuMb} className='menu-mobile flex items-end fixed left-0 right-0 top-0 bottom-0 w-full bg-white z-20 duration-500 translate-y-min'><Menu classStyle="menu-bar-mobile" /></div>
         </div>
     )
